e198ff2cc7d94aba753e00e197a5396979880b78
aiwake <yok@yok.yok>
2023-07-04 01:57:12 +0300

    train_test_split added

--- ML_iris.py
+++ ML_iris.py
@@ -49,8 +49,8 @@ from sklearn.model_selection import train_test_split
 X = features
 y = label
 
-
-
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.33,random_state=42)
+#Test_Data 33% seperated
 
 
 # %%
