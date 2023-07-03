34b1b16d48fbf2ad393bfa69bb84c84c0248cdc5
aiwake <yok@yok.yok>
2023-07-03 23:08:20 +0300

    features type

--- ML_iris.py
+++ ML_iris.py
@@ -3,9 +3,9 @@ from sklearn.datasets import load_iris
 
 irisSet = load_iris() #import iris dataset my project
 
-features = irisSet.data
-
-print(features)
+features = irisSet.data #sepal L sepal W lepal L lepal W include
+                        #features is (numpy.ndarray) array
+print(type(features))
 
 
 
