015bc068d71a6e5c5812e72a6bd76abc7f2be83e
aiwake <yok@yok.yok>
2023-07-03 23:15:54 +0300

    labelName added

--- ML_iris.py
+++ ML_iris.py
@@ -1,15 +1,19 @@
 # %%
+
+# 1-LOAD DATASET
 from sklearn.datasets import load_iris
 
 irisSet = load_iris()  # import iris dataset my project
 
-features = irisSet.data  # sepal L sepal W lepal L lepal W include
+features = irisSet.data  # sepal L sepal W lepal L lepal W Value include
 # features is (numpy.ndarray) array
 
 featuresName = irisSet.feature_names
 # ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
 
 label = irisSet.target  # target include 0 1 2 total 150 index
-print(label[-6:])
+labelName = irisSet.target_names  # ['setosa' 'versicolor' 'virginica']
+
+print(labelName)
 
 # %%
