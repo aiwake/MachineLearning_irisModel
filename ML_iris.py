d9a5bc7e6a65b87e79143d86429b9cf818180bda
aiwake <yok@yok.yok>
2023-07-03 23:13:26 +0300

    label add and look at content

--- ML_iris.py
+++ ML_iris.py
@@ -1,15 +1,15 @@
 # %%
 from sklearn.datasets import load_iris
 
-irisSet = load_iris() #import iris dataset my project
+irisSet = load_iris()  # import iris dataset my project
 
-features = irisSet.data #sepal L sepal W lepal L lepal W include
-                        #features is (numpy.ndarray) array
+features = irisSet.data  # sepal L sepal W lepal L lepal W include
+# features is (numpy.ndarray) array
+
 featuresName = irisSet.feature_names
+# ['sepal length (cm)', 'sepal width (cm)', 'petal length (cm)', 'petal width (cm)']
 
-print(featuresName)
-
-
-
+label = irisSet.target  # target include 0 1 2 total 150 index
+print(label[-6:])
 
 # %%
