073d289b015c727e3c657a4ee7335c14b843bdb0
aiwake <yok@yok.yok>
2023-07-04 02:01:59 +0300

    machine learning accuracy

--- ML_iris.py
+++ ML_iris.py
@@ -57,3 +57,11 @@ X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.33,random_state
 #6-TRAİN MODEL :
 
 claf.fit(X_train,y_train)
+
+rateTrain = claf.score(X_train,y_train)
+rateTest = claf.score(X_test, y_test)
+print("Test Data Rate:",rateTest,"%")
+print("Train Data Rate:",rateTrain,"%")
+
+
+
