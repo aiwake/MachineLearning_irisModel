c1cce4c9e4cf15618f6287f01861de3106905e44
aiwake <yok@yok.yok>
2023-07-04 15:34:12 +0300

    fast api add-2

--- server.py
+++ server.py
@@ -5,8 +5,8 @@
 
 # %%
 from joblib import load
-filename="aiwakeIrisModel.joblib"
-clafServ = load(filename)
+filename = "aiwakeIrisModel.joblib"
+clafUploaded = load(filename)
 
 
 # %%
@@ -18,12 +18,14 @@ app = FastAPI() # i linking this object and uvicorn
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Work": "Worked"}
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 #i start uvicorn this server
+#Anaconda Powershell Prompt open > cd YOUR_USER_NAME\anaconda3\envs\YOUR_ENV_NAME\YOUR_PROJECT_FILE
 #Anaconda Powershell Prompt open > uvicorn server:app --reload
 #Anaconda Powershell Prompt open > uvicorn this_file_name:app_16_line --reload
