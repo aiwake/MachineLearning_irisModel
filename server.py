a1e3805bf8ceb213e7a68e37fe4e247e7062d918
aiwake <yok@yok.yok>
2023-07-04 21:08:18 +0300

    v0.2

--- server.py
+++ server.py
@@ -1,34 +1,37 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 """
 @author: aiwake
 """
 
 # %%
 from joblib import load
-
 filename = "aiwakeIrisModel.joblib"
 clafUploaded = load(filename)
 
+
 # %%
 from typing import Union
-from fastapi import FastAPI, Request
+from fastapi import FastAPI , Request
 from fastapi.templating import Jinja2Templates
 
-# Create templates files your projets locate
+#Create templates files your projets locate
 templates = Jinja2Templates(directory="templates")
 
-app = FastAPI()  # i linking this object and uvicorn
+
+
+
+app = FastAPI() # i linking this object and uvicorn
 
 
 @app.get("/")
 async def read_root(request: Request):
-    return templates.TemplateResponse("item.html", {"request": request, "id": id})
+    return  templates.TemplateResponse("home.html", {"request": request})
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-# i start uvicorn this server
-# Anaconda Powershell Prompt open > uvicorn server:app --reload
-# Anaconda Powershell Prompt open > uvicorn this_file_name:app_16_line --reload
+#i start uvicorn this server
+#Anaconda Powershell Prompt open > uvicorn server:app --reload
+#Anaconda Powershell Prompt open > uvicorn this_file_name:app_16_line --reload
